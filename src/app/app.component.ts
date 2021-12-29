5a7fbb0584d2bec66fce3caba23326cd9684fafc
Pablo Salvi <salvipablo@gmail.com>
2021-12-29 16:54:40 -0300

    Agregado de servicios y consumo de api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { TaskService } from './services/task.service';
+import { PersonaService } from './services/persona.service';
+import { Persona } from './interfaces/persona';
+import { Response } from './interfaces/response';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,84 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebaAngPersona';
+  persons: Persona[];
+  person: Persona;
+  thatLooks: number = 0;
+  response: Response;
+
+  constructor(
+    private taskService: TaskService, 
+    private personaService: PersonaService
+  )
+  {
+    this.persons = [];
+
+    this.person = {
+      id: 0,
+      nombre: '-',
+      apellido: '-',
+      edad: 0
+    };
+
+    this.response = {
+      statusCode: 0,
+      description: '-'
+    };
+
+    this.getAllPersons();
+  }
+  
+  getAllPersons() {
+    this.personaService.getAllPersons().subscribe(persons => {
+      this.persons = persons;
+      this.thatLooks = 0;  // All people can be seen.
+    })
+  }
+
+  getPerson() {
+    this.personaService.getPerson().subscribe(person => {
+      console.log(person);
+      this.person = person;
+      this.thatLooks = 1;  // The searched person is displayed.
+    })
+  }
+
+  createPerson() {
+    const newPerson = {
+      nombre: 'Jorgue Luis',
+      apellido: 'Borges',
+      edad: 41
+    };
+    
+    this.thatLooks = 2;  // Person created.
+
+    this.personaService.createPerson(newPerson).subscribe((data) => {
+      this.response = data;
+    })
+
+    
+  }
+
+  updatePerson() {
+    const updatePerson = {
+      id: 9,
+      nombre: 'NombreEditado',
+      apellido: 'Martines',
+      edad: 22
+    };
+
+    this.thatLooks = 3;  // Person updated.
+
+    this.personaService.updatePerson(updatePerson, 9).subscribe((data) => {
+      console.log(data);
+    })
+  }
+
+  deletePerson() {
+    this.thatLooks = 4;  // Person eliminated.
+
+    this.personaService.deletePerson(7).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/services/persona.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Persona } from '../interfaces/persona';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  private api = 'http://localhost:8080/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllPersons() {
+    const path = `personas/traer`;
+    return this.http.get<Persona[]>(path)
+  }
+
+  getPerson() {
+    const path = `personas/traer/1`;
+    return this.http.get<Persona>(path)
+  }
+
+  createPerson(person: Persona): Observable<any> {
+    const path = `personas/crear`;
+    return this.http.post(path, person);
+  }
+
+  updatePerson(person: Persona, id: number) {
+
+    //http://localhost:8080/personas/editar/12?nombre=Jeremias&apellido=Amestoy&edad=35
+
+    const path = `personas/editar/${id}?nombre=${person.nombre}&apellido=${person.apellido}&edad=${person.edad}`;
+    return this.http.put<Task>(path, person);
+  }
+
+  deletePerson(id: number) {
+    const path = `personas/borrar/${id}`;
+    return this.http.delete(path);
+  }
+}
